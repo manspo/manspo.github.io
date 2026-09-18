efbd8b6ce0cd12fd76925b6e899b3339f0f7539d
AnonimPOGU <anonimpogu@gmail.com>
2026-09-18 05:46:28 +0300

    Новые серии

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'capsule-images-v1';
+const CACHE_NAME = 'capsule-images-v2';
 
 // ===== ТОЛЬКО ИЗОБРАЖЕНИЯ СЕРИЙ + ЛОГО + ABOUT =====
 self.addEventListener('install', event => {
